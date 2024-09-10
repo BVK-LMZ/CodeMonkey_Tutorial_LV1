a1872bbb57e7e1ca34302f0d4ac7a658afcf51a5
LMZ <78233321+BVK-LMZ@users.noreply.github.com>
2024-09-09 22:44:04 -0400

    Update Part_2:Variables&DataTypes.cs

--- Content/Part_2:Variables&DataTypes.cs
+++ Content/Part_2:Variables&DataTypes.cs
@@ -1,1 +1,25 @@
-y
+using System;
+
+class SomeProgram
+{
+    static void Main(string[] args)
+    {
+        // Declare different types of variables
+        int myInt = 42;
+        double myDouble = 3.14159;
+        string myString = "Hello, C#";
+        bool myBool = true;
+        char myChar = 'A';
+        float myFloat = 9.81f;
+        decimal myDecimal = 10000.55m;
+
+        // Print the variables with descriptions
+        Console.WriteLine("Integer: " + myInt);
+        Console.WriteLine("Double: " + myDouble);
+        Console.WriteLine("String: " + myString);
+        Console.WriteLine("Boolean: " + myBool);
+        Console.WriteLine("Character: " + myChar);
+        Console.WriteLine("Float: " + myFloat);
+        Console.WriteLine("Decimal: " + myDecimal);
+    }
+}
