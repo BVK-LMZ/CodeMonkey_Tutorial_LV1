818843a56169b120c646971e08d0ad4aa84ef229
LMZ <78233321+BVK-LMZ@users.noreply.github.com>
2024-09-09 22:49:44 -0400

    Update Part_3:If-ElseStatements.cs

--- Content/Part_3:If-ElseStatements.cs
+++ Content/Part_3:If-ElseStatements.cs
@@ -1,1 +1,22 @@
+using System;
 
+class SomeProgram
+{
+    static void Main(string[] args)
+    {
+        bool _someBool = false;
+
+        if (_someBool)
+        {
+            Console.WriteLine("True");
+        }
+        else if (!_someBool)
+        {
+            Console.WriteLine("False");
+        }
+        else
+        {
+            Console.WriteLine("Other");
+        }
+    }
+}
